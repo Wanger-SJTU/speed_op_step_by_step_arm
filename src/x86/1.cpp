029336beda4b650950353498b6aa40694811bc71
wanger <howiechen@sjtu.edu.cn>
2022-03-28 10:11:10 +0800

    fix error

--- src/x86/1.cpp
+++ src/x86/1.cpp
@@ -12,19 +12,18 @@ void matmul(float *a, float *b, float *c, int m, int n, int k)
 {
     for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < n; j+=4)
+        for (int j = 0; j < n; j += 4)
         {
             for (int r = 0; r < k; r++)
             {
-                C(i, j) += A(i, r) * B(r, j+0);
-                C(i, j) += A(i, r) * B(r, j+1);
-                C(i, j) += A(i, r) * B(r, j+2);
-                C(i, j) += A(i, r) * B(r, j+3);
+                C(i, j + 0) += A(i, r) * B(r, j + 0);
+                C(i, j + 1) += A(i, r) * B(r, j + 1);
+                C(i, j + 2) += A(i, r) * B(r, j + 2);
+                C(i, j + 3) += A(i, r) * B(r, j + 3);
             }
         }
     }
 }
-
 
 int main()
 {
--- src/x86/2.cpp
+++ src/x86/2.cpp
@@ -16,25 +16,25 @@ void matmul(float *a, float *b, float *c, int m, int n, int k)
         {
             for (int r = 0; r < k; r++)
             {
-                C(i, j) += A(i, r) * B(r, j + 0);
-                C(i, j) += A(i, r) * B(r, j + 1);
-                C(i, j) += A(i, r) * B(r, j + 2);
-                C(i, j) += A(i, r) * B(r, j + 3);
+                C(i, j+ 0) += A(i, r) * B(r, j + 0);
+                C(i, j+ 1) += A(i, r) * B(r, j + 1);
+                C(i, j+ 2) += A(i, r) * B(r, j + 2);
+                C(i, j+ 3) += A(i, r) * B(r, j + 3);
 
-                C(i + 1, j) += A(i + 1, r) * B(r, j + 0);
-                C(i + 1, j) += A(i + 1, r) * B(r, j + 1);
-                C(i + 1, j) += A(i + 1, r) * B(r, j + 2);
-                C(i + 1, j) += A(i + 1, r) * B(r, j + 3);
+                C(i + 1, j+ 0) += A(i + 1, r) * B(r, j + 0);
+                C(i + 1, j+ 1) += A(i + 1, r) * B(r, j + 1);
+                C(i + 1, j+ 2) += A(i + 1, r) * B(r, j + 2);
+                C(i + 1, j+ 3) += A(i + 1, r) * B(r, j + 3);
 
-                C(i + 2, j) += A(i + 2, r) * B(r, j + 0);
-                C(i + 2, j) += A(i + 2, r) * B(r, j + 1);
-                C(i + 2, j) += A(i + 2, r) * B(r, j + 2);
-                C(i + 2, j) += A(i + 2, r) * B(r, j + 3);
+                C(i + 2, j+ 0) += A(i + 2, r) * B(r, j + 0);
+                C(i + 2, j+ 1) += A(i + 2, r) * B(r, j + 1);
+                C(i + 2, j+ 2) += A(i + 2, r) * B(r, j + 2);
+                C(i + 2, j+ 3) += A(i + 2, r) * B(r, j + 3);
 
-                C(i + 3, j) += A(i + 3, r) * B(r, j + 0);
-                C(i + 3, j) += A(i + 3, r) * B(r, j + 1);
-                C(i + 3, j) += A(i + 3, r) * B(r, j + 2);
-                C(i + 3, j) += A(i + 3, r) * B(r, j + 3);
+                C(i + 3, j + 0) += A(i + 3, r) * B(r, j + 0);
+                C(i + 3, j + 1) += A(i + 3, r) * B(r, j + 1);
+                C(i + 3, j + 2) += A(i + 3, r) * B(r, j + 2);
+                C(i + 3, j + 3) += A(i + 3, r) * B(r, j + 3);
             }
         }
     }
