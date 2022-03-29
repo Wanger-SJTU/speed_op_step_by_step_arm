e0498e625f0c37d069d9dabf7bfd745be2655444
wanger <howiechen@sjtu.edu.cn>
2022-03-29 10:15:09 +0800

    update

--- src/x86/main.cpp
+++ src/x86/main.cpp
@@ -13,7 +13,8 @@ int main()
         a = new float[mkn * mkn];
         b = new float[mkn * mkn];
         c = new float[mkn * mkn];
-        double gflops = (2 * (double)mkn * mkn * mkn ) * 1e-9;
+        double gflops = (2 * (double)mkn * mkn * mkn) * 1e-9;
+        std::cout << "mkn:" << mkn << " FLOPs:" << gflops << flush;
         auto time_span = std::chrono::duration<double, std::micro>::max().count();
         for (int i = 0; i < 10; ++i)
         {
@@ -23,8 +24,7 @@ int main()
             std::chrono::duration<double, std::micro> dur = end - start;
             time_span = dur.count() < time_span ? dur.count() : time_span;
         }
-        std::cout << "mkn:" << mkn << " FLOPs:" << gflops
-                  << " time:" << time_span << " GFLOPS:" << gflops / (time_span /1000000) << endl;
+        std::cout << " time:" << time_span << " GFLOPS:" << gflops / (time_span /1000000) << endl;
         delete a;
         delete b;
         delete c;
