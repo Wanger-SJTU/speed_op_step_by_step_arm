69fadfff7fd05d579a609d700b3a5c1d4d58757c
wanger <howiechen@sjtu.edu.cn>
2023-04-16 16:59:16 +0800

    arm neon GLOPS

--- src/armv8a/gflops/test.sh
+++ src/armv8a/gflops/test.sh
@@ -1,6 +1,11 @@
 
+aarch64-linux-android30-clang -c neon.s -o neon.o
+aarch64-linux-android30-clang -c gflops_main.c -o main.o
+aarch64-linux-android30-clang  main.o neon.o -o main
 
-
-adb push main /data/tmp/main
+adb push main /data/local/tmp/main
 # cat sys/devices/system/cpu/cpu7/cpufreq/scaling_available_frequencies 
-adb shell "cd /data/tmp/ && ./main"
+adb shell "cd /data/local/tmp/ && ./main"
+
+rm *.o 
+rm main
