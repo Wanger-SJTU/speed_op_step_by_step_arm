38b17dd8df0ce83774886474e1cd9903f8d5d5b4
wanger <howiechen@sjtu.edu.cn>
2023-04-21 21:14:48 +0800

    cnth for sve

---
+++ src/armv8a/micro_kernel_test/main.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+// aarch64-linux-android30-clang main.c -o main -march=armv8a+sve
+
+void print_cnth() 
+{
+    register int x asm("x0");
+    x = 0;
+    printf("%d\n", x);
+    
+    asm volatile(
+        "cnth x0"
+        : "=x"(x)
+        : "0"(x)
+        :
+    );
+
+    printf("%d\n", x);
+}
+
+int main() {
+
+}
